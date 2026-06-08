f4f4dacb97c1a9b29c0c5f5a5ed8970f31834d2b
Suman Acharya <acharyasuman874@gmail.com>
2026-06-08 19:59:10 +0545

    Add RootLayout component for application layout

---
+++ app/layout.js
@@ -0,0 +1,8 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ne">
+      <head><title>भूमि प्रशासन सेवा पोर्टल</title></head>
+      <body style={{ margin: 0, padding: 0, background: '#f4f7f6' }}>{children}</body>
+    </html>
+  );
+}
