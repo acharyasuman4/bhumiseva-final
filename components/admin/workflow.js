6dd3a8ad1c6e27a4c2926bcb31f7a27af66eb6f9
Suman Acharya <acharyasuman874@gmail.com>
2026-06-11 22:29:11 +0545

    Create workflow.js

---
+++ components/admin/workflow.js
@@ -0,0 +1,7 @@
+export default function Placeholder({ label }) {
+  return (
+    <div style={{padding: '50px', textAlign: 'center', background: '#fff', borderRadius: '15px'}}>
+       <h3>{label} सेक्सन तयार हुँदैछ...</h3>
+    </div>
+  );
+}
